7804a5e71ebc7b891a70a69f90b4cb891d166b9e
Tyler P <48499703+tylerpiccolo@users.noreply.github.com>
2020-09-18 13:53:20 -0500

    Add documentation and examples

--- 02_Formatting/main.go
+++ 02_Formatting/main.go
@@ -1,12 +1,21 @@
-/* 
+/*
 https://gobyexample.com/string-formatting
+https://golang.org/pkg/fmt/
 
-formatting string. 
+Formatting strings
 - go offers several printing verbs to format values
--- eg. %d, %v, %o, %X, %p, %q, %s, %E, ...
+-- %d 	integer
+-- %f 	float
+-- %t 	boolean
+-- %c 	char code
+-- %x 	hex code
+-- %b 	binary
+-- %e	scientific notation
+-- %s 	string
+-- %v 	default value. Useful for float, int, and bool
 
-...
-
+and many others:
+-- %o, %X, %p, %q, %s, %E, %v...
 */
 
 package main
@@ -15,14 +24,22 @@ import "fmt"
 
 func main() {
 
-	i := 42 
+	i := 42
 	fmt.Printf(`
 	%d
 	%T 
 	%c
 	%b
 	%#X
-	%o`, i,i,i,i,i,i)
+	%o`, i, i, i, i, i, i)
+
+	// Alternatively written
+	fmt.Printf("%d\n%T\n%c\n%b\n%#X\n%o", i, i, i, i, i, i)
+
+	str := "Hello"
+	str1 := "World"
+
+	fmt.Print("\n\n", str+str1, i, "\n")
+	fmt.Println(i + i)
+
 }
-
-
