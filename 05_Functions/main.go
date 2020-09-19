b74665addc79ee9c6b18c1e339e6cfd417d8e286
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2020-09-19 17:49:10 +0000

    change functions file

--- 05_Functions/main.go
+++ 05_Functions/main.go
@@ -1,49 +1,65 @@
 /*
-	Functions, et al.
+	Functions 
 
-	Returning multiple values
+	First class citizens
+	- Then assigned to variables and passed as arguments to other functions and can be returned from a function. 
+	- Functions can return multiple variables	
+	
+	
+	Functions can be stored as struct fields.
+
 	Variadic functions 
 	-- accepts any number of arguments in a form of a slice or array
 	Slice parameters
 	Anon func
-
 */
 
 package main 
 
+import (
+	"fmt"
+)
 
-// 
 
-
-
-
-import "fmt"
-
-func sum(si ...float64) (int, float64) {
-	var length int
-	var total float64
-
-	for _, v := range si {
-		length += 1
-		total += float64(v)
-	}
-	return length, total
+func print1(f func()) {
 }
+
+// func print2(f func()) {
+// 	f()
+// }
 
 func main() {
 
-	nums := []float64{1,2,3,4,5,6,7,8,9}
-	length, total := sum(nums...)
-	
-	var average float64 = total / float64(length)
-	fmt.Println(average)
 
-	// anon func
-	var x int = 42
-	increment := func() int {
-		x++
-		return x
-	}
-
-	fmt.Println(increment())
 }
+
+
+
+
+// the ...type notation unfurls container types (eg slice, array, maps)
+// func sum(si ...float64) (int, float64) {
+// 	var length int
+// 	var total float64
+
+// 	// Blank Identifier notation _
+// 	// used for readability, and 
+// 	for _, v := range si {
+// 		length += 1
+// 		total += float64(v)
+// 	}
+// 	return length, total
+// }
+
+
+	// nums := []float64{1,2,3,4,5,6,7,8,9}
+	// length, total := sum(nums...)
+	
+	// var average float64 = total / float64(length)
+	// fmt.Println(average)
+
+	// // anon func
+	// var x int = 42
+	// increment := func() int {
+	// 	x++
+	// 	return x
+	// }
--- 10_Install_Packages/main.go
+++ 10_Install_Packages/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	// run: `go install` to install dependencies in local pkg
-	helper "github.com/cmd-ctrl-q/401GoProject/helper/stringutil"
+	helper "src/github.com/cmd-ctrl-q/401GoProject/helper/stringutil"
+
+
 )
 
 
 func main() {
 
 	fmt.Println(helper.Reverse("pay pao"))
+
 }
